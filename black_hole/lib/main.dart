2b67706b66e596c9402a08ca99249e84638b20dd
it21183454 <99382245+shanulkshan@users.noreply.github.com>
2024-03-11 17:05:13 +0530

    adding scrollview and comments to sections

--- black_hole/lib/main.dart
+++ black_hole/lib/main.dart
@@ -24,21 +24,32 @@ class MyApp extends StatelessWidget {
               IconButton(onPressed: () {},icon: const Icon(Icons.menu),),
             ],
           ),
-          body:  Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-              const Text("SPACE DETAILS", style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),),
-              SizedBox(height: 50,),
-              Center(
-                child: Image.asset("assets/space1.png", 
-                height: 300,
-                scale: 2,),
-              ),
-              SizedBox(height: 50,),
-              const Text("lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.w400),),
-            ],),
+          body:  SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+
+                //text
+                const Text("SPACE DETAILS", style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),),
+               const SizedBox(height: 50,),
+
+                //image
+                Center(
+                  child: Image.asset("assets/space1.png", 
+                  height: 300,
+                  scale: 2,),
+                ),
+               const SizedBox(height: 50,),
+
+                //text
+                const Text("lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.w400),),
+
+                //button
+                
+              ],),
+            ),
           ),
         ),
       ),
